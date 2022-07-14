4d2ba29951d46438ea21f0440c14403f43d9a6b9
zhabiev1 <mzhabiev@gmail.com>
2022-07-14 19:24:26 +0200

    view and url

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Главная страница</h1>")
+
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
+
